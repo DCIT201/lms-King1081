d64996a3abbfc8acdbd9f5272bbfbff330433da6
King1081 <papahquansah@gmail.com>
2024-12-13 23:15:04 +0000

    First Commit

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,14 +1,41 @@
-/*
- * This should be your main class where all your objects will be created
- */
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Library library = new Library();
+        Patron patron1 = new Patron("Papah", "P001");
+        Patron patron2 = new Patron("Kweku", "P002");
+        Patron patron3 = new Patron("Okae", "P003");
+
+
+        // Add books
+        Book book1 = new Book("Training Your Dragon", "Astrid Van Helsing", "10812947");
+        Book book2 = new Book("How To Be A Fuckboy", "Stan Lee", "22033299");
+        library.addBook(book1);
+        library.addBook(book2);
+
+        // List available books
+        library.listAvailableBooks();
+
+        // Checkout a book
+        library.checkoutBook("10812947", patron1);
+
+        // Attempt to checkout the same book
+        library.checkoutBook("10812947", patron2);
+
+        // List available books after checkout
+        library.listAvailableBooks();
+
+        // Return a book
+        library.returnBook("10812947", patron1);
+
+        // List available books after return
+        library.listAvailableBooks();
+
+        // Checkout another book
+        library.checkoutBook("22033299", patron3);
+
+        // List available books after checkout
+        library.listAvailableBooks();
     }
 }
